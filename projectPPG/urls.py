8d329ca9547d2849597274de80d6f1d7fc6c4aa2
Bouyahi Mohamed <mohamed.bouyahi@sesame.com.tn>
2025-04-03 10:37:52 +0100

    configure media file to upload image

--- projectPPG/urls.py
+++ projectPPG/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Ecommerce.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files in development
+# The static() function is used to serve static files during development.
