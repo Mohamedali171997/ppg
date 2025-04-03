a9de79cd2cd0bb687288fdda4e1028861f1e226f
Bouyahi Mohamed <mohamed.bouyahi@sesame.com.tn>
2025-04-03 15:22:55 +0100

    add __str__ method to Rating model for better string representation

--- Ecommerce/models.py
+++ Ecommerce/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Rating(models.Model):
     image = models.ImageField(upload_to='ratings/')
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
