c8d4e11f7d21496b8ebdd625203cfb3a1c1f4141
Bouyahi Mohamed <mohamed.bouyahi@sesame.com.tn>
2025-04-19 00:20:34 +0100

    add signup functionality with user registration and styling updates

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('product/<pk>/', views.product_detail, name='product_detail'),
+    path('signup/', views.signup, name='signup'),
     
 ]
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect
 from .models import Product,Rating
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
     products = Product.objects.all()
@@ -38,3 +40,25 @@ def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
  
     return render(request, 'product_detail.html', {'product': product})
+def signup(request):
+    form = SignUpForm()
+
+    if request.method == 'POST':
+
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful')
+            return redirect('home')
+        else:
+            messages.error(request, 'Registration failed')
+            return redirect('signup')
+    else:
+        form = SignUpForm()
+    # form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+    
